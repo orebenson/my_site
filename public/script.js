5af8a527fcf9defa55bc7bf706769f92bb2e9d73
osbson <orebenson@hotmail.com>
2023-06-10 22:30:24 +0100

    Changed to one page

--- public/script.js
+++ public/script.js
@@ -1,53 +1,28 @@
 const homePage = document.getElementById("homeLink")
-const projectsPage = document.getElementById("projectsLink")
-
-const aboutLink = document.getElementById("aboutLink")
 const skillsLink = document.getElementById("skillsLink")
 const contactLink = document.getElementById("contactLink")
+const projectsLink = document.getElementById("projectsLink")
 
-const project1Link = document.getElementById("project1Link")
-const project2Link = document.getElementById("project2Link")
-// const project3Link = document.getElementById("project3Link")
 
-function setActive (active_div, div2, div3=null) {
-    active_div.className = "active"
-    div2.className = "inactive"
-    div3.className = "inactive"
+function setActive (active_div, other_divs) {
+    active_div.className = "active";
+    for (const div of other_divs) {
+        div.className = "inactive";
+    }
 }
 
 homePage.addEventListener('click', (e) => {
-    setActive(homePage, projectsPage)
-    // setActive(project1Link, project2Link, project3Link)
+    setActive(homePage, [skillsLink, contactLink, projectsLink])
 })
-
-projectsPage.addEventListener('click', (e) => {
-    setActive(projectsPage, homePage)
-    // setActive(project1Link, project2Link, project3Link)
-})
-
-aboutLink.addEventListener('click', (e) => {
-    setActive(aboutLink, skillsLink, contactLink)
-})
-
 skillsLink.addEventListener('click', (e) => {
-    setActive(skillsLink, aboutLink, contactLink)
+    setActive(skillsLink, [homePage, contactLink, projectsLink])
 })
-
 contactLink.addEventListener('click', (e) => {
-    setActive(contactLink, skillsLink, aboutLink)
+    setActive(contactLink, [skillsLink, homePage, projectsLink])
+})
+projectsLink.addEventListener('click', (e) => {
+    setActive(projectsLink, [skillsLink, contactLink, homePage])
 })
 
-project1Link.addEventListener('click', (e) => {
-    setActive(project1Link, project2Link)
-    // setActive(project1Link, project2Link, project3Link)
-})
 
-project2Link.addEventListener('click', (e) => {
-    setActive(project2Link, project1Link)
-    // setActive(project2Link, project1Link, project3Link)
-})
-
-// project3Link.addEventListener('click', (e) => {
-//     setActive(project3Link, project1Link, project2Link)
-// })
 
